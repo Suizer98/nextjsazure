ab1e1786dd9c3f6c49f0e0434770cd7db439448e
Sui Zer <teysuizer1998@gmail.com>
2024-03-18 15:50:08 +0800

    added middleware

---
+++ middleware/_middleware.ts
@@ -0,0 +1,7 @@
+// middleware/_middleware.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { basicAuthentication } from './basicAuthentication'; // Ensure this utility is implemented
+
+export function middleware(request: NextRequest) {
+  return basicAuthentication(request);
+}
