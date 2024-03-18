413a72b12bc92bf114a72edb98b9505c775257ab
Sui Zer <teysuizer1998@gmail.com>
2024-03-18 16:13:44 +0800

    Removed unused middlware and app folder

--- middleware/_middleware.ts
+++
@@ -1,7 +0,0 @@
-// middleware/_middleware.ts
-import { NextRequest, NextResponse } from "next/server";
-import { basicAuthentication } from "./basicAuthentication"; // Ensure this utility is implemented
-
-export function middleware(request: NextRequest) {
-  return basicAuthentication(request);
-}
