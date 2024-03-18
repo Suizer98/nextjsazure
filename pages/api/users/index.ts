5a5e59e65701bde142e04b51163195ee2a910963
Sui Zer <teysuizer1998@gmail.com>
2024-03-18 15:57:19 +0800

    Added cors for frontend app

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,9 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import nextCors from 'nextjs-cors'; // Import the `nextjs-cors` library
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Run the CORS middleware
+  await nextCors(req, res, {
+    // Options
+    methods: ['GET', 'POST'], // Specify which methods to allow
+    origin: "https://suizerlyciawedding.netlify.app", // Specify the origin to allow
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  
   // Handle POST requests - Create a new user
   if (req.method === 'POST') {
     const { name, pax, allergic } = req.body;
@@ -20,13 +29,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(500).json({ error: 'Failed to create user' });
     }
   } 
-  // Handle GET requests - List all users
+  // Handle GET requests - List all users, but require a correct 'key' query parameter
   else if (req.method === 'GET') {
-    try {
-      const users = await prisma.user.findMany();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to retrieve users' });
+    const { key } = req.query; // Extract the 'key' query parameter
+    const correctKey = process.env.PW; // The expected key stored in an environment variable
+
+    // Proceed only if the 'key' query parameter exists and matches the expected key
+    if (key && key === correctKey) {
+      try {
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
+      } catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve users' });
+      }
+    } else {
+      // If 'key' is missing or does not match, respond with 401 Unauthorized
+      res.status(401).json({ error: 'Unauthorized: Access requires a valid API key' });
     }
   } 
   // Respond with 405 Method Not Allowed if the request method is not supported
