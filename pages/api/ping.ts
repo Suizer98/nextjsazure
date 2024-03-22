a97f4a2c5ae053be5a85a7f0b499cd523c4150f7
Sui Zer <suizer@nus.edu.sg>
2024-03-22 13:13:28 +0800

    Added ping ts

---
+++ pages/api/ping.ts
@@ -0,0 +1,21 @@
+// pages/api/pingDB.ts
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    // Perform your database ping operation here
+    await (prisma as any).$queryRaw`SELECT 1`;
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error pinging database:", error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to ping database" });
+  }
+}
