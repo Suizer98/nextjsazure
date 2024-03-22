34beba079fcf5c11f3446f0e387bfafb37d58a3c
Suizer <93068036+Suizer98@users.noreply.github.com>
2024-03-22 17:17:54 +0800

    Update entrypoint.sh to capture env from outer container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 set -e
 service ssh start
+
+# Export environment variables from the outer container to the inner one
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
 exec npm run dev
