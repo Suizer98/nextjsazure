47f8c0ce85174b5d1d43d385ee90d4cfaf78dd71
Sui Zer <teysuizer1998@gmail.com>
2024-05-28 10:13:50 +0800

    Replaced vector layer with vector tile to avoid flashing issuue

--- app/components/MapView.client.tsx
+++ app/components/MapView.client.tsx
@@ -3,9 +3,9 @@ import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import GeoJSON from "ol/format/GeoJSON";
+import VectorTileLayer from "ol/layer/VectorTile";
+import VectorTileSource from "ol/source/VectorTile";
+import MVT from "ol/format/MVT";
 import OSM from "ol/source/OSM";
 import { fromLonLat } from "ol/proj";
 import { defaults as defaultControls } from "ol/control";
@@ -25,9 +25,9 @@ const MapView = () => {
   const [sidebarExpanded, setSidebarExpanded] = useState(false);
   const [map, setMap] = useState<Map | null>(null);
   const [osmLayer, setOsmLayer] = useState<TileLayer<OSM> | null>(null);
-  const [vectorLayer, setVectorLayer] =
-    useState<VectorLayer<VectorSource> | null>(null);
-  const [windLayer, setWindLayer] = useState<any>();
+  const [vectorTileLayer, setVectorTileLayer] =
+    useState<VectorTileLayer | null>(null);
+  const [windLayer, setWindLayer] = useState<any>(null);
   const [layersVisible, setLayersVisible] = useState<LayersVisible>({
     osm: true,
     vector: true,
@@ -42,32 +42,10 @@ const MapView = () => {
       source: new OSM(),
     });
 
-    const initialVectorLayer = new VectorLayer({
-      source: new VectorSource({
-        features: new GeoJSON().readFeatures(
-          {
-            type: "FeatureCollection",
-            features: [
-              {
-                type: "Feature",
-                geometry: {
-                  type: "LineString",
-                  coordinates: [
-                    [103.8198, 1.3521], // Singapore
-                    [102, 5.9749],
-                  ],
-                },
-                properties: {
-                  name: "Singapore to Sabah",
-                },
-              },
-            ],
-          },
-          {
-            dataProjection: "EPSG:4326",
-            featureProjection: "EPSG:3857",
-          },
-        ),
+    const initialVectorTileLayer = new VectorTileLayer({
+      source: new VectorTileSource({
+        format: new MVT(),
+        url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.pbf",
       }),
     });
 
@@ -76,19 +54,7 @@ const MapView = () => {
       windOptions: {
         velocityScale: 1 / 100,
         paths: 5000,
-        colorScale: (velocity: number) => {
-          // if (velocity <= 1) return '#FFFFFF';  // White
-          // else if (velocity > 1 && velocity <= 2) return '#ADD8E6'  // Light Blue
-          // else if (velocity > 2 && velocity <= 3) return '#32CD32'  // Lime Green
-          // else if (velocity > 3 && velocity <= 4) return '#FFD700'  // Gold
-          // else if (velocity > 4 && velocity <= 5) return '#FF8C00'  // Dark Orange
-          // else if (velocity > 5 && velocity <= 6) return '#FF4500' // Orange Red
-          // else if (velocity > 6 && velocity <= 7) return '#DC143C'  // Crimson
-          // else if (velocity > 7 && velocity <= 8) return '#C71585'  // Medium Violet Red
-          // else if (velocity > 8 && velocity <= 9) return '#8B0000'  // Dark Red
-          // else return '#B22222'  // Firebrick
-          return "#FF4500";
-        },
+        colorScale: (velocity: number) => "#FF4500",
         width: 3,
         generateParticleOption: false,
       },
@@ -96,7 +62,7 @@ const MapView = () => {
 
     const initialMap = new Map({
       target: mapElementRef.current,
-      layers: [initialOsmLayer, initialVectorLayer, initialWindLayer],
+      layers: [initialOsmLayer, initialVectorTileLayer, initialWindLayer],
       view: new View({
         center: fromLonLat([103.5, 1.5]),
         zoom: 8,
@@ -106,7 +72,7 @@ const MapView = () => {
 
     setMap(initialMap);
     setOsmLayer(initialOsmLayer);
-    setVectorLayer(initialVectorLayer);
+    setVectorTileLayer(initialVectorTileLayer);
     setWindLayer(initialWindLayer);
 
     return () => initialMap.setTarget(undefined);
@@ -114,9 +80,9 @@ const MapView = () => {
 
   useEffect(() => {
     if (osmLayer) osmLayer.setVisible(layersVisible.osm);
-    if (vectorLayer) vectorLayer.setVisible(layersVisible.vector);
+    if (vectorTileLayer) vectorTileLayer.setVisible(layersVisible.vector);
     if (windLayer) windLayer.setVisible(layersVisible.wind);
-  }, [layersVisible, osmLayer, vectorLayer, windLayer]);
+  }, [layersVisible, osmLayer, vectorTileLayer, windLayer]);
 
   const toggleSidebar = () => {
     setSidebarExpanded(!sidebarExpanded);
