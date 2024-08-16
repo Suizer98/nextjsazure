4ffb66fdd7f1804edb78ae10a50be7d96012faef
Suizer98 <93068036+Suizer98@users.noreply.github.com>
2024-08-16 23:41:11 +0800

    Tried out NextUI with sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { Checkbox } from '@nextui-org/checkbox'
+import { Button } from '@nextui-org/react'
 import React, { useEffect, useState } from 'react'
 
 type SidebarProps = {
@@ -44,13 +46,13 @@ const Sidebar: React.FC<SidebarProps> = ({
 
   return (
     <div>
-      <button
+      <Button
         onClick={toggleSidebar}
         className="absolute top-4 left-4 z-50 bg-white border border-gray-300 rounded-md p-2 flex items-center justify-center"
         aria-label="Toggle Sidebar"
       >
         {sidebarExpanded ? '▶' : '◀'}
-      </button>
+      </Button>
       <div
         className={`transition-all duration-500 overflow-y-auto shadow-md bg-white ${
           sidebarExpanded ? 'w-60' : 'w-0'
@@ -59,57 +61,45 @@ const Sidebar: React.FC<SidebarProps> = ({
         {sidebarExpanded && (
           <div className="p-4 pt-16">
             <div className="flex flex-col space-y-4">
-              <label className="text-sm font-medium text-gray-700">
-                This is a client side rendered react component. The main goal of this website is to
-                showcase complete workflow of auto deploy pipeline using:
-              </label>
-              <label className="text-sm text-gray-700">1. Github Action pipeline</label>
-              <label className="text-sm text-gray-700">
+              <p className="text-sm font-medium text-gray-700">
+                This is a client-side rendered React component. The main goal of this website is to
+                showcase the complete workflow of auto deploy pipeline using:
+              </p>
+              <p className="text-sm text-gray-700">1. GitHub Action pipeline</p>
+              <p className="text-sm text-gray-700">
                 2. Docker login and push to Azure Container Registry
-              </label>
-              <label className="text-sm text-gray-700">
-                3. Deploy image as web app on Azure Web App Service
-              </label>
+              </p>
+              <p className="text-sm text-gray-700">
+                3. Deploy image as a web app on Azure Web App Service
+              </p>
               <div className="flex items-center justify-between">
                 <label htmlFor="osm-toggle" className="text-sm font-medium text-gray-700">
                   OSM Layer
                 </label>
-                <input
-                  id="osm-toggle"
-                  type="checkbox"
-                  checked={layersVisible.osm}
+                <Checkbox
+                  isSelected={layersVisible.osm}
                   onChange={() => setLayersVisible((prev) => ({ ...prev, osm: !prev.osm }))}
-                  className="toggle-checkbox"
                 />
               </div>
               <div className="flex items-center justify-between">
                 <label htmlFor="vector-toggle" className="text-sm font-medium text-gray-700">
                   Vector Layer
                 </label>
-                <input
-                  id="vector-toggle"
-                  type="checkbox"
-                  checked={layersVisible.vector}
+                <Checkbox
+                  isSelected={layersVisible.vector}
                   onChange={() =>
                     setLayersVisible((prev) => ({
                       ...prev,
                       vector: !prev.vector
                     }))
                   }
-                  className="toggle-checkbox"
                 />
               </div>
               <div className="flex items-center justify-between">
                 <label htmlFor="wind-toggle" className="text-sm font-medium text-gray-700">
                   Wind Layer
                 </label>
-                <input
-                  id="wind-toggle"
-                  type="checkbox"
-                  checked={layersVisible.wind}
-                  onChange={handleWindLayerToggle}
-                  className="toggle-checkbox"
-                />
+                <Checkbox isSelected={layersVisible.wind} onChange={handleWindLayerToggle} />
               </div>
             </div>
           </div>
