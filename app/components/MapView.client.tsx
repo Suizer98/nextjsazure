01b9780b9fc69d4047dbb86eb2f57e2117c86171
Suizer <93068036+Suizer98@users.noreply.github.com>
2025-01-18 11:52:03 +0800

    Update MapView center to more focus on Singapore center

--- app/components/MapView.client.tsx
+++ app/components/MapView.client.tsx
@@ -74,7 +74,7 @@ const MapView: React.FC<MapViewProps> = ({ sidebarExpanded, toggleSidebar }) => 
       target: mapElementRef.current,
       layers: [initialOsmLayer, initialVectorLayer, initialWindLayer],
       view: new View({
-        center: fromLonLat([103.5, 1.5]),
+        center: fromLonLat(103.8198, 1.3521]),
         zoom: 8
       }),
       controls: defaultControls({ zoom: false })
